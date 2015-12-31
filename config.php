e5b7d1d0563d13888beafb96a979e9699e5f0eb2
cahyadsn <cahyadsn@gmail.com>
2015-12-31 08:29:29 +0700

    database configuration file

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+/*********************************
+FILENAME    : config.php
+CREATE BY   : cahya dsn
+PURPOSE     : database configuration
+CREATE DATE : 2013-01-20
+UPDATE DATE : 2015-12-31
+**********************************/
+$dbhost='localhost';
+$dbuser='root';
+$dbpass='';
+$dbname='expert';
+$db=new mysqli($dbhost,$dbuser,$dbpass,$dbname);
